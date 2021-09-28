56c9b19e92449c9c1d45b419001a210590583c42
XFPM <xavier@drmorris.com.au>
2021-09-29 07:55:58 +1000

    Initial commit

---
+++ ass2.py
@@ -0,0 +1,50 @@
+#%%
+from scipy.integrate import quad
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.optimize as so
+
+#%%
+print('''
+Question 1 b
+''')
+r_1 = 10
+
+f = lambda r,r0: 1/(r**2 * np.sqrt( 1/r0**2*(1-2/r0) -  1/r**2*(1-2/r)))
+
+
+def findZ(input_r):
+    y,err = quad(f, input_r, r_1, args=(input_r,))
+    return y - np.pi/2
+
+
+r_0 = so.fsolve(findZ, 5)[0]
+
+print('r_0 = {:.2f}'.format(r_0))
+
+#%%
+print('''
+Question 1 c
+''')
+
+ib2 = 1/r_0**2*(1-2/r_0)
+
+ib = np.sqrt(ib2)
+
+tfunc = lambda r: ib*(1-2/r)**(-1)*( ib2 - 1/r**2*(1-2/r))**(-1/2)
+
+t_0, e = quad(tfunc, r_1, r_0)
+t_1, e = quad(tfunc, r_0, r_1)
+t_2, e = quad(tfunc, r_1, r_0)
+t_3, e = quad(tfunc, r_0, r_1) 
+
+
+print('Travel Time Schwarzschild: {:.2f}'.format( - t_0 + t_1 - t_2 + t_3))
+
+#%%
+print('''
+Question 1 d
+''')
+t_flat_segment = np.sqrt(r_0**2 + r_1**2)
+print('Travel Time Flat: {:.2f}'.format(t_flat_segment*4))
+print('Flat < Schwarzschild: {}'.format(t_flat_segment*4 < - t_0 + t_1 - t_2 + t_3))
